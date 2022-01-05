65adaa8e014bec00e6301260a13c2d2e51e7ce01
praj31 <rajputpraj.31@gmail.com>
2022-01-05 12:28:23 +0530

    nav link and seo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,18 @@ export const Navbar: React.FunctionComponent = () => {
 	return (
 		<nav>
 			<h1>Tokenizer</h1>
-			<div>GitHub</div>
+			<div>
+				<a
+					href='https://github.com/praj31/tokenizer'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					<img
+						src='https://super.so/icon/light/github.svg'
+						alt='GitHub logo'
+					/>
+				</a>
+			</div>
 		</nav>
 	)
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Tokenizer</title>
+				<meta
+					name='description'
+					content='Utility application to generate random IDs or tokens.'
+				/>
+				<meta charSet='UTF-8'></meta>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1.0'
+				></meta>
+
+				<meta property='og:title' content='Tokenizer' />
+				<meta
+					property='og:description'
+					content='Utility application to generate random IDs or tokens.'
+				/>
+				<meta property='og:type' content='website' />
+				<meta property='og:image' content='/favicon.png' />
+				<meta property='og:image:width' content='1024' />
+				<meta property='og:image:height' content='1024' />
+				<meta property='og:site_name' content='Pratikraj_Rajput' />
+				<link
+					rel='icon'
+					type='image/svg+xml'
+					href='/favicon.svg'
+				></link>
+				<link rel='icon' type='image/png' href='/favicon.png'></link>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
