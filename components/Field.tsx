342c375528c478244027e87f3300b271695682a0
praj31 <rajputpraj.31@gmail.com>
2022-01-05 10:26:21 +0530

    copied state css

--- components/Field.tsx
+++ components/Field.tsx
@@ -1,25 +1,50 @@
+import { useEffect, useState } from 'react'
+
 interface Props {
 	title: string
 }
 
 export const Field: React.FunctionComponent<Props> = ({ title }) => {
+	const [copied, setCopied] = useState<boolean>(false)
+
+	useEffect(() => {
+		if (copied) {
+			setTimeout(() => {
+				setCopied(false)
+			}, 5000)
+		}
+	}, [copied])
+
+	const generate = () => {
+		setCopied(false)
+	}
+
 	return (
 		<div className='field-wrapper'>
 			<div className='field-header'>
 				<p>{title}</p>
 				<div className='field-action-button'>
-					<button>Generate</button>
+					<button onClick={generate}>Generate</button>
 				</div>
 			</div>
 			<div className='field-body'>
 				<div>
-					<input type='text' />
-					<button>
-						<img
-							src='https://super.so/icon/dark/clipboard.svg'
-							alt='Clipboard Icon'
-							id='clipboard-icon'
-						/>
+					<input id='field-input' type='text' />
+					<button
+						className={
+							copied ? 'field-button-active' : 'field-button'
+						}
+						id='field-button'
+						onClick={() => setCopied(true)}
+					>
+						{!copied && (
+							<img
+								src='https://super.so/icon/dark/clipboard.svg'
+								alt='Clipboard Icon'
+								id='clipboard-icon'
+							/>
+						)}
+						{copied && <p className='copied-message'>Copied!</p>}
 					</button>
 				</div>
 			</div>
